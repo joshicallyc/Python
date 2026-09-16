428482108ff01080a29131f187dd22cf7f4360da
joshicallyc <joshicallyc@gmail.com>
2026-09-16 12:24:22 -0700

    Delete hello_world directory

--- hello_world/basics/while_loop.py
+++
@@ -1,9 +0,0 @@
-# number = 100
-# while number > 0:
-#     print(number)
-#     number //= 2
-
-command = "" #MAKE SURE TO RUN THIS IN TERMINAL 
-while command.lower() != "quit": #in this case it doesn't matter how u type "quit" it will terminate teh program
-    command = input(">")
-    print("ECHO", command)
